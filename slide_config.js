1435f4bf9a71a11008c6d44611b50c448343d860
acastemoreno <acastemoreno@gmail.com>
2016-09-07 09:39:51 -0500

    avance

---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'Paradigmas - Gestion de la calidad', 
+     subtitle: 'Docente: Ing. Silva Wilson Jose', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/uni.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'Albert Castellano', 
+     company: '', 
+     gplus: '', 
+     twitter: '', 
+     www: '', 
+     github: '', 
+   }]
+};
